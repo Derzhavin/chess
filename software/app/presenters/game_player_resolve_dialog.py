46e9d02e90847534f04d3c3a6b3b482aeee9c566
Derzhavin <derzhavind@bk.ru>
2022-05-12 22:19:51 +0300

    improved chess player resolve dialog

--- software/app/presenters/game_player_resolve_dialog.py
+++ software/app/presenters/game_player_resolve_dialog.py
@@ -1,6 +1,10 @@
+from datetime import date
+
 from PyQt5 import QtWidgets, uic
 from enum import Enum
 
+from PyQt5.QtCore import QItemSelectionModel
+from PyQt5.QtWidgets import QAbstractItemView
 from app.models.qt_models import GamePlayerTableModel
 
 
@@ -44,38 +48,62 @@ class GamePlayerResolveDialog(QtWidgets.QDialog):
         self.choose_player_page = ChoosePlayerPage(self, config)
         self.stacked_widget.addWidget(self.choose_player_page)
 
-        # self.create_player_page = CreatePlayerPage(self, config)
-        # self.stacked_widget.addWidget(self.create_player_page)
+        self.create_player_page = CreatePlayerPage(self, config)
+        self.stacked_widget.addWidget(self.create_player_page)
 
         self.selected_option = GamePlayerResolveDialog.SelectedOptionStep.cancel_op
-        self.select_opt_page.push_button_next.clicked.connect(self.on_push_button_next_clicked)
-        self.select_opt_page.radio_button_cancel_operation.toggled.connect(self.on_radio_button_toggled)
-        self.select_opt_page.radio_button_choose_player.toggled.connect(self.on_radio_button_toggled)
-        self.select_opt_page.radio_button_create_player.toggled.connect(self.on_radio_button_toggled)
+        self.select_opt_page.push_button_next.clicked.connect(self.on_select_opt_page_push_button_next_clicked)
+        self.select_opt_page.radio_button_cancel_operation.toggled.connect(self.on_select_opt_page_radio_button_toggled)
+        self.select_opt_page.radio_button_choose_player.toggled.connect(self.on_select_opt_page_radio_button_toggled)
+        self.select_opt_page.radio_button_create_player.toggled.connect(self.on_select_opt_page_radio_button_toggled)
 
-        self.go_to_select_page()
-        self.show()
+        self.choose_player_page.push_button_accept.clicked.connect(self.on_choose_player_page_push_button_accept_clicked)
+        self.choose_player_page.push_button_back.clicked.connect(self.on_choose_player_page_push_button_back_clicked)
 
-    def go_to_select_page(self):
+        self.create_player_page.push_button_accept.clicked.connect(self.on_create_player_page_push_button_accept_clicked)
+        self.create_player_page.push_button_back.clicked.connect(self.on_create_player_page_push_button_back_clicked)
+
+        self.create_player_page.text_edit_first_name.textChanged.connect(self.on_create_player_page_text_edit_first_name_changed)
+        self.create_player_page.text_edit_last_name.textChanged.connect(self.on_create_player_page_text_edit_last_name_changed)
+        self.create_player_page.text_edit_date_of_birth.textChanged.connect(self.on_create_player_page_text_edit_date_of_birth_changed)
+        self.create_player_page.plaintext_edit_comment.textChanged.connect(self.on_create_player_page_plaintext_edit_comment_changed)
+
+        self.go_to_select_opt_page()
+
+        self.target_chess_player_first_name = ''
+        self.target_chess_player_last_name = ''
+        self.target_chess_player_date_of_birth = date.today()
+        self.target_chess_player_comment = ''
+
+    def go_to_select_opt_page(self):
         self.stacked_widget.setCurrentIndex(0)
 
     def go_to_choose_player_page(self):
         self.stacked_widget.setCurrentIndex(1)
 
+        game_player_table_model = GamePlayerTableModel(self.game_player_repo, self.criterion)
+
+        self.choose_player_page.table_view_game_players.setModel(game_player_table_model)
+        self.choose_player_page.table_view_game_players.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.choose_player_page.table_view_game_players.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        index = game_player_table_model.index(0, 0)
+        selection_model = self.choose_player_page.table_view_game_players.selectionModel()
+        selection_model.select(index, QItemSelectionModel.ClearAndSelect | QItemSelectionModel.Rows)
+
     def go_to_create_player_page(self):
         self.stacked_widget.setCurrentIndex(2)
 
-    def on_push_button_next_clicked(self):
+    def on_select_opt_page_push_button_next_clicked(self):
         if self.selected_option == GamePlayerResolveDialog.SelectedOptionStep.cancel_op:
             self.close()
             return
         if self.selected_option == GamePlayerResolveDialog.SelectedOptionStep.choose_player_op:
             self.go_to_choose_player_page()
-            self.choose_player_page.table_view_game_players.setModel(GamePlayerTableModel(self.game_player_repo, self.criterion))
-        elif self.selected_option == GamePlayerResolveDialog.SelectedOptionStep.choose_player_op:
+        elif self.selected_option == GamePlayerResolveDialog.SelectedOptionStep.create_player_op:
             self.go_to_create_player_page()
 
-    def on_radio_button_toggled(self):
+    def on_select_opt_page_radio_button_toggled(self):
         if self.select_opt_page.radio_button_cancel_operation.isChecked():
             self.selected_option = GamePlayerResolveDialog.SelectedOptionStep.cancel_op
         elif self.select_opt_page.radio_button_choose_player.isChecked():
@@ -83,3 +111,26 @@ class GamePlayerResolveDialog(QtWidgets.QDialog):
         else:
             self.selected_option = GamePlayerResolveDialog.SelectedOptionStep.create_player_op
 
+    def on_create_player_page_push_button_accept_clicked(self):
+        self.accept()
+
+    def on_create_player_page_push_button_back_clicked(self):
+        self.go_to_select_opt_page()
+
+    def on_choose_player_page_push_button_accept_clicked(self):
+        self.accept()
+
+    def on_choose_player_page_push_button_back_clicked(self):
+        self.go_to_select_opt_page()
+
+    def on_create_player_page_text_edit_first_name_changed(self, first_name: str):
+        self.target_chess_player_first_name = first_name
+
+    def on_create_player_page_text_edit_last_name_changed(self, last_name: str):
+        self.target_chess_player_first_name = last_name
+
+    def on_create_player_page_text_edit_date_of_birth_changed(self, date_of_birth: str):
+        self.target_chess_player_first_name = date.fromisoformat(date_of_birth)
+
+    def on_create_player_page_plaintext_edit_comment_changed(self, comment: str):
+        self.target_chess_player_comment = comment
--- software/app/services/pgn_import_service.py
+++ software/app/services/pgn_import_service.py
@@ -6,15 +6,11 @@ from app.store import ChessGame, ChessPlayer
 from app.presenters import GamePlayerResolveDialog
 
 from sqlalchemy import and_
-from typing import TypeVar, Generic
-
-ChessGameRepoT = TypeVar('ChessGameRepoT')
-ChessPlayerRepoT = TypeVar('ChessPlayerRepoT')
 
 
 class PgnImportService:
 
-    def __init__(self, engine, chess_game_repo_cls: ChessGameRepoT, game_player_repo_cls: ChessPlayerRepoT):
+    def __init__(self, engine, chess_game_repo_cls, game_player_repo_cls):
         self.session = sessionmaker(engine)()
         self.chess_game_repo = chess_game_repo_cls(self.session)
         self.game_player_repo = game_player_repo_cls(self.session)
@@ -32,11 +28,20 @@ class PgnImportService:
         criterion = and_(ChessPlayer.first_name == first_name, ChessPlayer.last_name == last_name)
 
         if self.game_player_repo.exists(criterion):
-            if not GamePlayerResolveDialog(parent_widget, config, self.game_player_repo, criterion).exec():
+            game_player_resolve_dialog = GamePlayerResolveDialog(parent_widget, config, self.game_player_repo, criterion)
+            if not game_player_resolve_dialog.exec():
                 self.session.rollback()
                 return False
             else:
-                pass
+                if game_player_resolve_dialog.selected_option == GamePlayerResolveDialog.SelectedOptionStep.choose_player_op:
+                    pass
+                elif game_player_resolve_dialog.selected_option ==  GamePlayerResolveDialog.SelectedOptionStep.create_player_op:
+                    white_player = ChessPlayer(
+                        game_player_resolve_dialog.target_chess_player_first_name,
+                        game_player_resolve_dialog.target_chess_player_last_name,
+                        game_player_resolve_dialog.target_chess_player_date_of_birth,
+                        game_player_resolve_dialog.target_chess_player_comment
+                    )
         else:
             white_player = ChessPlayer(first_name, last_name, None, '')
             self.game_player_repo.add_player(white_player)
@@ -45,9 +50,20 @@ class PgnImportService:
         criterion = and_(ChessPlayer.first_name == first_name, ChessPlayer.last_name == last_name)
 
         if self.game_player_repo.exists(criterion):
-            if not GamePlayerResolveDialog(parent_widget, config, self.game_player_repo).exec():
+            game_player_resolve_dialog = GamePlayerResolveDialog(parent_widget, config, self.game_player_repo, criterion)
+            if not game_player_resolve_dialog.exec():
                 self.session.rollback()
                 return False
+            else:
+                if game_player_resolve_dialog.selected_option == GamePlayerResolveDialog.SelectedOptionStep.choose_player_op:
+                    pass
+                elif game_player_resolve_dialog.selected_option == GamePlayerResolveDialog.SelectedOptionStep.create_player_op:
+                    black_player = ChessPlayer(
+                        game_player_resolve_dialog.target_chess_player_first_name,
+                        game_player_resolve_dialog.target_chess_player_last_name,
+                        game_player_resolve_dialog.target_chess_player_date_of_birth,
+                        game_player_resolve_dialog.target_chess_player_comment
+                    )
         else:
             black_player = ChessPlayer(first_name, last_name, None, '')
             self.game_player_repo.add_player(black_player)
